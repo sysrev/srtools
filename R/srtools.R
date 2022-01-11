383755e45acfc6141c0e1a5a97bc090c3038eca5
Thomas Luechtefeld <tomluec@gmail.com>
2022-01-11 17:46:42 -0500

    a simple vignette showing how to download data.

--- R/srtools.R
+++ R/srtools.R
@@ -1,22 +1,20 @@
-#' download pdfs
-#' @param dois digital object identifiers
-#' @param dir the directory to store pdfs
-#' @importFrom rlang .data
-#' @return
+#' en_ern
+#' @description
+#' get `electronic-resource-num` from endnote records
+#' @importFrom xml2 as_list read_xml
+#' @param xmlrecord a <record>...</record> entry from xml
+#' @return tibble with <ern> column
 #' @export
-#' @examples
-#' download.pdfs(c("10.1186/s12864-016-2566-9","10.1103/physreve.88.012814"))
-download_pdfs = function(dois,email,dir=tempdir()){
-  chunks = split(dois, ceiling(seq_along(dois)/100))
-
-  get.pdf.urls = purrr::slowly(\(ids){
-    roadoi::oadoi_fetch(dois=ids,email=email) |> tidyr::unnest(.data$best_oa_location) |> dplyr::select(.data$doi,.data$url_for_pdf) },
-    rate = purrr::rate_delay(1))
-
-  tb    = dplyr::bind_rows(pbapply::pblapply(chunks,get.pdf.urls)) |> dplyr::mutate(file=glue::glue("{basename(url_for_pdf)}.pdf"))
-  res   = download.file(tb$url_for_pdf,destfile = glue::glue("{dir}/{tb$file}"))
-  files = list.files(dir)
-
-  tb |> dplyr::mutate(downloaded = .data$file %in% files)
+en.ern = function(xmlrecord){
+  xml2::read_xml(xmlrecord) |>
+    xml2::xml_find_all("electronic-resource-num") |>
+    xml2::xml_text()
 }
 
+#' check if a string is a DOI
+#' @description
+#' lots of work to do here
+#' @param string the string to check
+#' @return true or false
+#' @export
+is_doi = function(string){ grepl("^10.\\d{4,9}/[-._;()/:A-Z0-9]+$",string,ignore.case = T) }
